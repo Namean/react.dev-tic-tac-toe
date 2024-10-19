6f7968c6d0a0867ce65ae3ee0baa36633570b3cc
dev-capitan <dwilson@capitanappliance.com>
2024-10-19 10:23:10 -0400

    completed raising up state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-
 import Square from "./components/Square.component";
 
-export default function Board() {
-    return (
-        <>
-          <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-          </div>
-          <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-          </div>
-          <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-          </div>
-        </>
-      );
+function handleClick() {
+  const nextSquares = squares.slice();
+  nextSquares[0] = "X";
+  setSquares(nextSquares);
 }
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
+      </div>
+    </>
+  );
+}
+onSquareClick = { handleClick };
